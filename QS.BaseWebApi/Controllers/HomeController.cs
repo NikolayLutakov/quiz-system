2e509acd17c8f66def59fe684b468d269f6891c7
Nikolay Lyutakov <nikolay.lyutakov@egt-digital.com>
2022-08-11 16:33:43 +0300

    add 'Shared' project

---
+++ QS.BaseWebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QS.BaseWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            return new string[] { "Index 1" };
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Index2()
+        {
+            return new string[] { "Index 2" };
+        }
+    }
+}
