1c3390a608adf127e7af9d2c342e45b8639ad21a
Nikolay Lyutakov <nikolay.lyutakov@egt-digital.com>
2022-08-11 17:01:44 +0300

    configure Identity Server

--- QS.Authentication/Config.cs
+++ QS.Authentication/Config.cs
@@ -11,16 +11,48 @@ namespace QS.Authentication
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId()
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
-            { };
+            {
+                new ApiScope("qsBaseWebApi")
+            };
 
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+            new Client[]
+            {
+                 new Client
+                 {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "qsBaseWebApi" }
+                 },
+
+                  new Client
+                    {
+                        ClientId = "postman",
+                        AllowedGrantTypes = GrantTypes.Code,
+                        RequirePkce = true,
+                        RequireClientSecret = false,
+                        AllowedCorsOrigins = { "https://oauth.pstmn.io" },
+                        AllowedScopes = { "qsBaseWebApi" },
+                        RedirectUris = { "https://oauth.pstmn.io/v1/callback" },
+                        PostLogoutRedirectUris = { "https://oauth.pstmn.io/v1/callback/" },
+                        Enabled = true
+                    }
+            };
     }
 }
--- QS.Authentication/Startup.cs
+++ QS.Authentication/Startup.cs
@@ -20,9 +20,6 @@ namespace QS.Authentication
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // uncomment, if you want to add an MVC-based UI
-            //services.AddControllersWithViews();
-
             var builder = services.AddIdentityServer(options =>
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
@@ -31,9 +28,6 @@ namespace QS.Authentication
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients);
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
         }
 
         public void Configure(IApplicationBuilder app)
